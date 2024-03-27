e4652df9a5b3a2ef923fc1aaaf47ee5b1965b6e5
Marcos Onofre <marcosxrzz@gmail.com>
2024-03-26 22:07:47 -0300

    Inital Commit

--- app/src/main/java/com/example/calcula_viagem/MainActivity.kt
+++ app/src/main/java/com/example/calcula_viagem/MainActivity.kt
@@ -17,35 +17,48 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.btnCalcula.setOnClickListener(this)
+        binding.btnCalculate.setOnClickListener(this)
 
-        closedKeyboard()
 
     }
 
     override fun onClick(view: View) {
-        if (view.id == R.id.btn_Calcula){
-            Toast.makeText(this, "Botão Funcionando", Toast.LENGTH_SHORT).show()
+        if (view.id == R.id.btn_Calculate) {
+            calculate()
         }
     }
 
+  private fun isValid():Boolean {
+
+      return (binding.edtDistance.text.toString() != ""
+              && binding.edtPrice.text.toString() != ""
+              && binding.edtAutonomy.text.toString() != ""
+              && binding.edtAutonomy.text.toString().toFloat() != 0f)
+  }
+
     private fun calculate() {
 
-    }
+        if (isValid()){
+            val distance = binding.edtDistance.text.toString().toFloat()
+            val price = binding.edtPrice.text.toString().toFloat()
+            val autonomy = binding.edtAutonomy.text.toString().toFloat()
 
-    /*
+            val totalValue = (distance * price) / autonomy
+            binding.txtTotalValue.text = "R$ ${"%.2f".format(totalValue)}"
 
-    -> MODO JAVA
-    private void ocutarTeclado() {
-        InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
-        inputMethodManager.hideSoftInputFromWindow(edtValor.getWindowToken(), InputMethodManager.HIDE_NOT_ALWAYS);
+            closedKeyboard()
+
+        } else {
+            Toast.makeText(this, "Preencha todos os campos.", Toast.LENGTH_SHORT).show()
+
+        }
+
     }
-    */
 
     private fun closedKeyboard() {
         val closedKeyboard = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         closedKeyboard.hideSoftInputFromWindow(
-            binding.edtPreco.windowToken,
+            binding.btnCalculate.windowToken,
             InputMethodManager.HIDE_NOT_ALWAYS
         )
     }
